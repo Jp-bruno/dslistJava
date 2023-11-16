375ed70b1e977f113f1158a5da85a3cc4d970fc9
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-11-16 19:42:20 -0300

    domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -0,0 +1,68 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPK {
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	private Game game;
+	
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	private GameList list;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Game getGame() {
+		return game;
+	}
+	public void setGame(Game game) {
+		this.game = game;
+	}
+	public GameList getList() {
+		return list;
+	}
+	public void setList(GameList list) {
+		this.list = list;
+	}
+	 
+	public BelongingPK() {
+		
+	}
+	
+	public BelongingPK(Game game, GameList list) {
+		this.game = game;
+		this.list = list;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if(obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+	}
+	
+	
+	
+}
